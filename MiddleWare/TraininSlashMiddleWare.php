31d31420551402c01725cb3ee2d68d0a291d0e1c
nfouka <nadir.fouka@gmail.com>
2020-10-10 21:56:04 +0200

    update middlew	are

--- MiddleWare/TraininSlashMiddleWare.php
+++ MiddleWare/TraininSlashMiddleWare.php
@@ -9,7 +9,7 @@ use Psr\Http\Message\ServerRequestInterface;
 class TraininSlashMiddleWare {
     public function __invoke(ServerRequestInterface $request, ResponseInterface $response, callable $next){
         $url = (string) $request->getUri()->getPath() ;
-        if(!empty($url) && $url[-1] === '/'){
+        if(!empty($url) &&  str_ends_with($url,"/") ){
             return $response
                 ->withHeader('Location' , substr($url,0,-1))
                 ->withStatus(301) ;
